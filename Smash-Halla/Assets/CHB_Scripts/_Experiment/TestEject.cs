324bc995b37f3e94d45d28b96dc77cfa66872cce
Charles BENTEIN <c.bentein@rubika-edu.com>
2021-03-30 12:08:07 +0200

    Wha il ya une ejection

---
+++ Smash-Halla/Assets/CHB_Scripts/_Experiment/TestEject.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Experiment
+{
+	public class TestEject : MonoBehaviour
+	{
+		#region Variables
+		public Rigidbody2D dummyRb;
+		public Transform projectPoint;
+		Vector2 eject;
+		public float ejectMultiply = 1f;
+        #endregion
+
+        private void Start()
+        {
+			//dummyRb = GetComponent<Rigidbody2D>();
+			ejectMultiply = 33f;
+        }
+        // Update is called once per frame
+        void Update()
+		{
+            if (Input.GetKeyDown(KeyCode.T))
+            {
+				eject = projectPoint.position - transform.position;
+				dummyRb.AddForce(eject * ejectMultiply, ForceMode2D.Impulse);
+				ejectMultiply += 6.35f;
+            }
+		}
+	}
+}
+
